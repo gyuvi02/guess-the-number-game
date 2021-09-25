8ad2228e0e7575ec9feb3c1f800ddf1bbed2860e
gyuvi02 <Gyulus99>
2021-09-25 23:27:50 +0200

    Adding the project files

---
+++ core/src/main/java/org/gyula/Main.java
@@ -0,0 +1,13 @@
+package org.gyula;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+
+    }
+}
