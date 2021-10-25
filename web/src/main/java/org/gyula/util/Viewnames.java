5b9f566c83a63c905f4aab83a785219cbd64cf2f
gyuvi02 <Gyulus99>
2021-10-25 10:24:53 +0200

    First Thymeleaf view added

---
+++ web/src/main/java/org/gyula/util/Viewnames.java
@@ -0,0 +1,11 @@
+package org.gyula.util;
+
+public final class Viewnames {
+
+    // constants
+    public static final String HOME = "home";
+
+    // constructor - private, so it cannot be instantiated
+    private Viewnames() {}
+
+}
