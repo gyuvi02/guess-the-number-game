d556f8facb63c9cba4de66bd56f465da4a723afd
gyuvi02 <Gyulus99>
2021-11-02 08:48:54 +0100

    Interceptors added

---
+++ web/src/main/java/org/gyula/interceptor/RequestInterceptor.java
@@ -0,0 +1,37 @@
+package org.gyula.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("preHandle method called. handler = {}", handler);
+        log.debug("URL = {}", request.getRequestURL());
+//        return HandlerInterceptor.super.preHandle(request, response, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.debug("postHandle method called. handler = {}", handler);
+        log.debug("URL = {}", request.getRequestURL());
+        log.debug("model = {}", modelAndView.getModel());
+        log.debug("view = {}", modelAndView.getViewName());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        log.debug("afterCompletion method called. handler = {}", handler);
+        log.debug("URL = {}", request.getRequestURL());
+
+    }
+}
